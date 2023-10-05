3cd6c31a18f26f39549fb1b34f2f22f2f64c0d84
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-10-05 19:12:58 +0300

    Oxford Comma Lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(elements):
+    if len(elements) == 1:
+        return elements[0]
+    elif len(elements) == 2:
+        return f"{elements[0]} and {elements[1]}"
+    else:
+        formatted_elements = ", ".join(elements[:-1])  # Join all elements except the last one with commas
+        return f"{formatted_elements}, and {elements[-1]}"  # Add "and" before the last element
